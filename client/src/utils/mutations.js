aafe68159288c4cd999c7ab5b51c2bf6cda7542f
Honorine Ndom <honorinendzah@gmail.com>
2022-04-15 06:52:56 -0700

    changes done on mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,3 @@
-import { gql } from '@apollo/client';
-
 export const ADD_THOUGHT = gql`
   mutation addThought($thoughtText: String!, $thoughtAuthor: String!) {
     addThought(thoughtText: $thoughtText, thoughtAuthor: $thoughtAuthor) {
@@ -10,6 +8,22 @@ export const ADD_THOUGHT = gql`
       comments {
         _id
         commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
       }
     }
   }
